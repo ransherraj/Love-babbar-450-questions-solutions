dd709fe82c5ff6957623529748d6c956b835977d
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2021-09-01 02:51:10 +0530

    Create 7. rotate array by one.cpp

---
+++ 7. rotate array by one.cpp
@@ -0,0 +1,48 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+void rotate(int arr[], int n);
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n] , i;
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        rotate(a, n);
+        for (i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+    }
+        return 0;
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+void rotate(int arr[], int n)
+{
+  
+  // ***************************  clockwise rotation  ***************************
+    int temp = arr[n-1];
+    for( int i = n-1; i>0; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0] = temp;
+  
+  // *****************************  Anticlockwise rotation **********************
+  
+//   int temp = 0;
+//     for( int i = 0; i<n-1; i--){
+//         arr[i] = arr[i+1];
+//     }
+//     arr[n-1] = temp;
+  
+}
